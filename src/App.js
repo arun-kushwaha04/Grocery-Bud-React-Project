0568d799f955473457743af7f7519a020777c51d
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-15 13:46:14 +0530

    Created Grocery form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name,setName] = useState('');
+  const [list,setList] = useState('');
+  const [alert,setAlert] = useState(false);
+  const [isEditing,setIsEditing] = useState(false);
+  const [editID,setEditID] = useState(null);
+  return <section className="section-center">
+    <form className="grocery-form">
+      {alert && <Alert/>}
+      <h3>Grocery List</h3>
+      <div className="form-control">
+        <input type="text"
+          className="grocery"
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+          placeholder="e.g. bread"
+        />
+        <button type="submit" className="submit-btn">
+          {isEditing?'Edit':'Add'}
+        </button>
+      </div>
+    </form>
+    <div className="grocery-container">
+      <List list={list}/>
+      <button className='clear-btn'>clear button</button>
+    </div>
+    </section>
 }
 
 export default App

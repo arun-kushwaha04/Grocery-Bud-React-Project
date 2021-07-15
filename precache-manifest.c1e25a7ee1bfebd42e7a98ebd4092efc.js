d579f7a1763a34014e3737f8f4bb6145f5a00792
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-15 15:03:23 +0530

    Updates

---
+++ precache-manifest.c1e25a7ee1bfebd42e7a98ebd4092efc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157ca148bc6655cea151e531f1856055",
+    "url": "/Grocery-Bud-React-Project/index.html"
+  },
+  {
+    "revision": "8e3a94e46b4f8f437fee",
+    "url": "/Grocery-Bud-React-Project/static/css/main.a903c182.chunk.css"
+  },
+  {
+    "revision": "a5640d4bcbc7f4184abc",
+    "url": "/Grocery-Bud-React-Project/static/js/2.79ac4408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery-Bud-React-Project/static/js/2.79ac4408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3a94e46b4f8f437fee",
+    "url": "/Grocery-Bud-React-Project/static/js/main.4ed30463.chunk.js"
+  },
+  {
+    "revision": "e8b8b5fde9bd309541ed",
+    "url": "/Grocery-Bud-React-Project/static/js/runtime-main.51840763.js"
+  }
+]);
